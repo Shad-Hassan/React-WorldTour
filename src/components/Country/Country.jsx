496f4db0fed1439bf0fead3f13a976a683a32a79
Shad Hassan <shadhassan8991@gmail.com>
2023-09-14 00:47:33 +0600

    first commit

---
+++ src/components/Country/Country.jsx
@@ -0,0 +1,12 @@
+import './Country.css'
+
+const Country = ({country}) => {
+    const {name} = country;
+    return (
+        <div className='country'>
+            <h3>Name: {name?.common}</h3>
+        </div>
+    );
+};
+
+export default Country;
